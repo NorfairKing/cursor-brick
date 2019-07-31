f3603dac03e39bc748d549bddcb0eedd087528f9
Tom Sydney Kerckhove <syd@cs-syd.eu>
2019-07-31 17:54:55 +0200

    horizontal and vertical forest cursor widgets

--- cursor-brick/src/Cursor/Brick/Forest.hs
+++ cursor-brick/src/Cursor/Brick/Forest.hs
@@ -13,6 +13,43 @@ import Brick.Widgets.Core
 
 import Cursor.Brick.List.NonEmpty
 import Cursor.Brick.Tree
+horizontalForestCursorWidgetM ::
+     Monad m
+  => (CTree b -> m (Widget n))
+  -> (TreeCursor a b -> m (Widget n))
+  -> (CTree b -> m (Widget n))
+  -> ForestCursor a b
+  -> m (Widget n)
+horizontalForestCursorWidgetM prevFunc curFunc nextFunc =
+  horizontalNonEmptyCursorWidgetM prevFunc curFunc nextFunc . forestCursorListCursor
+
+horizontalForestCursorWidget ::
+     (CTree b -> Widget n)
+  -> (TreeCursor a b -> Widget n)
+  -> (CTree b -> Widget n)
+  -> ForestCursor a b
+  -> Widget n
+horizontalForestCursorWidget prevFunc curFunc nextFunc =
+  horizontalNonEmptyCursorWidget prevFunc curFunc nextFunc . forestCursorListCursor
+
+verticalForestCursorWidgetM ::
+     Monad m
+  => (CTree b -> m (Widget n))
+  -> (TreeCursor a b -> m (Widget n))
+  -> (CTree b -> m (Widget n))
+  -> ForestCursor a b
+  -> m (Widget n)
+verticalForestCursorWidgetM prevFunc curFunc nextFunc =
+  verticalNonEmptyCursorWidgetM prevFunc curFunc nextFunc . forestCursorListCursor
+
+verticalForestCursorWidget ::
+     (CTree b -> Widget n)
+  -> (TreeCursor a b -> Widget n)
+  -> (CTree b -> Widget n)
+  -> ForestCursor a b
+  -> Widget n
+verticalForestCursorWidget prevFunc curFunc nextFunc =
+  verticalNonEmptyCursorWidget prevFunc curFunc nextFunc . forestCursorListCursor
 
 forestCursorWidgetM ::
      ([CTree b] -> TreeCursor a b -> [CTree b] -> m (Widget n)) -> ForestCursor a b -> m (Widget n)
