e5049b3f740fd7bbc274de2fd6198b4e387737a6
Tom Sydney Kerckhove <syd@cs-syd.eu>
2019-07-31 18:17:34 +0200

    Key value widgets

--- cursor-brick/src/Cursor/Brick/Map/KeyValue.hs
+++ cursor-brick/src/Cursor/Brick/Map/KeyValue.hs
@@ -13,8 +13,16 @@ import Brick.Widgets.Core
 
 import Cursor.Brick.List.NonEmpty
 
+keyValueWidget :: (kc -> v ->Widget n) -> (k -> vc -> Widget n) -> KeyValueCursor kc vc k v -> Widget n
+keyValueWidget = foldKeyValueCursor
+
+keyValueWidgetM :: (kc -> v ->f( Widget n)) -> (k -> vc -> f(Widget n)) -> KeyValueCursor kc vc k v -> f(Widget n)
+keyValueWidgetM = foldKeyValueCursor
+
+
 traverseKeyValueCursor :: (kc -> v -> f c) -> (k -> vc -> f c) -> KeyValueCursor kc vc k v -> f c
 traverseKeyValueCursor = foldKeyValueCursor
+
 foldKeyValueCursor :: (kc -> v -> c) -> (k -> vc -> c) -> KeyValueCursor kc vc k v -> c
 foldKeyValueCursor keyFunc valFunc kvc =
   case kvc of
