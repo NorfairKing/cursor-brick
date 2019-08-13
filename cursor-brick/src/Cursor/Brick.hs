94907e9ad1a2100fa0309733a58317c982e0f146
Tom Sydney Kerckhove <syd@cs-syd.eu>
2019-08-13 12:44:11 +0200

    bumped version and added exports

--- cursor-brick/src/Cursor/Brick.hs
+++ cursor-brick/src/Cursor/Brick.hs
@@ -1,7 +1,19 @@
 module Cursor.Brick
-  ( module Cursor.Brick.List
+  ( module Cursor.Brick.Forest
+  , module Cursor.Brick.List
   , module Cursor.Brick.List.NonEmpty
+  , module Cursor.Brick.Map
+  , module Cursor.Brick.Map.KeyValue
+  , module Cursor.Brick.Text
+  , module Cursor.Brick.TextField
+  , module Cursor.Brick.Tree
   ) where
 
+import Cursor.Brick.Forest
 import Cursor.Brick.List
 import Cursor.Brick.List.NonEmpty
+import Cursor.Brick.Map
+import Cursor.Brick.Map.KeyValue
+import Cursor.Brick.Text
+import Cursor.Brick.TextField
+import Cursor.Brick.Tree
