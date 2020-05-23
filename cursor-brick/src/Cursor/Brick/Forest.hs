22badad28c6fb281f569e6ef5b026fbdf606334b
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-05-24 01:24:48 +0200

    convenience function for drawing vertical tree and forest cursors

--- cursor-brick/src/Cursor/Brick/Forest.hs
+++ cursor-brick/src/Cursor/Brick/Forest.hs
@@ -1,65 +1,113 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Cursor.Brick.Forest where
 
+import Brick.Types
+import Brick.Widgets.Core
+import Cursor.Brick.List.NonEmpty
+import Cursor.Brick.Tree
 import Cursor.Forest
 import Cursor.Tree
-
-import Brick.Types
-
-import Cursor.Brick.List.NonEmpty
+import qualified Data.List.NonEmpty as NE
 
 horizontalForestCursorWidgetM ::
-     Monad m
-  => (CTree b -> m (Widget n))
-  -> (TreeCursor a b -> m (Widget n))
-  -> (CTree b -> m (Widget n))
-  -> ForestCursor a b
-  -> m (Widget n)
+  Monad m =>
+  (CTree b -> m (Widget n)) ->
+  (TreeCursor a b -> m (Widget n)) ->
+  (CTree b -> m (Widget n)) ->
+  ForestCursor a b ->
+  m (Widget n)
 horizontalForestCursorWidgetM prevFunc curFunc nextFunc =
-  horizontalNonEmptyCursorWidgetM prevFunc curFunc nextFunc .
-  forestCursorListCursor
+  horizontalNonEmptyCursorWidgetM prevFunc curFunc nextFunc
+    . forestCursorListCursor
 
 horizontalForestCursorWidget ::
-     (CTree b -> Widget n)
-  -> (TreeCursor a b -> Widget n)
-  -> (CTree b -> Widget n)
-  -> ForestCursor a b
-  -> Widget n
+  (CTree b -> Widget n) ->
+  (TreeCursor a b -> Widget n) ->
+  (CTree b -> Widget n) ->
+  ForestCursor a b ->
+  Widget n
 horizontalForestCursorWidget prevFunc curFunc nextFunc =
-  horizontalNonEmptyCursorWidget prevFunc curFunc nextFunc .
-  forestCursorListCursor
+  horizontalNonEmptyCursorWidget prevFunc curFunc nextFunc
+    . forestCursorListCursor
+
+verticalPaddedForestCursorWidgetM ::
+  forall a b n m.
+  Monad m =>
+  (a -> m (Widget n)) ->
+  (b -> m (Widget n)) ->
+  Int ->
+  ForestCursor a b ->
+  m (Widget n)
+verticalPaddedForestCursorWidgetM goA goB padding =
+  verticalForestCursorWidgetM
+    goCTree
+    (verticalPaddedTreeCursorWidgetM goA goB padding)
+    goCTree
+  where
+    goCTree :: CTree b -> m (Widget n)
+    goCTree (CNode b cf) = do
+      top <- goB b
+      bot <- goCForest cf
+      pure $ top <=> padLeft (Pad padding) bot
+    goCForest :: CForest b -> m (Widget n)
+    goCForest = \case
+      EmptyCForest -> pure emptyWidget
+      ClosedForest _ -> pure emptyWidget
+      OpenForest nect -> fmap vBox $ traverse goCTree $ NE.toList nect
+
+verticalPaddedForestCursorWidget ::
+  forall a b n.
+  (a -> Widget n) ->
+  (b -> Widget n) ->
+  Int ->
+  ForestCursor a b ->
+  Widget n
+verticalPaddedForestCursorWidget goA goB padding =
+  verticalForestCursorWidget
+    goCTree
+    (verticalPaddedTreeCursorWidget goA goB padding)
+    goCTree
+  where
+    goCTree :: CTree b -> Widget n
+    goCTree (CNode b cf) = goB b <=> padLeft (Pad padding) (goCForest cf)
+    goCForest :: CForest b -> Widget n
+    goCForest = \case
+      EmptyCForest -> emptyWidget
+      ClosedForest _ -> emptyWidget
+      OpenForest nect -> vBox $ map goCTree $ NE.toList nect
 
 verticalForestCursorWidgetM ::
-     Monad m
-  => (CTree b -> m (Widget n))
-  -> (TreeCursor a b -> m (Widget n))
-  -> (CTree b -> m (Widget n))
-  -> ForestCursor a b
-  -> m (Widget n)
+  Monad m =>
+  (CTree b -> m (Widget n)) ->
+  (TreeCursor a b -> m (Widget n)) ->
+  (CTree b -> m (Widget n)) ->
+  ForestCursor a b ->
+  m (Widget n)
 verticalForestCursorWidgetM prevFunc curFunc nextFunc =
-  verticalNonEmptyCursorWidgetM prevFunc curFunc nextFunc .
-  forestCursorListCursor
+  verticalNonEmptyCursorWidgetM prevFunc curFunc nextFunc
+    . forestCursorListCursor
 
 verticalForestCursorWidget ::
-     (CTree b -> Widget n)
-  -> (TreeCursor a b -> Widget n)
-  -> (CTree b -> Widget n)
-  -> ForestCursor a b
-  -> Widget n
+  (CTree b -> Widget n) ->
+  (TreeCursor a b -> Widget n) ->
+  (CTree b -> Widget n) ->
+  ForestCursor a b ->
+  Widget n
 verticalForestCursorWidget prevFunc curFunc nextFunc =
-  verticalNonEmptyCursorWidget prevFunc curFunc nextFunc .
-  forestCursorListCursor
+  verticalNonEmptyCursorWidget prevFunc curFunc nextFunc
+    . forestCursorListCursor
 
 forestCursorWidgetM ::
-     ([CTree b] -> TreeCursor a b -> [CTree b] -> m (Widget n))
-  -> ForestCursor a b
-  -> m (Widget n)
+  ([CTree b] -> TreeCursor a b -> [CTree b] -> m (Widget n)) ->
+  ForestCursor a b ->
+  m (Widget n)
 forestCursorWidgetM = foldForestCursor
 
 forestCursorWidget ::
-     ([CTree b] -> TreeCursor a b -> [CTree b] -> Widget n)
-  -> ForestCursor a b
-  -> Widget n
+  ([CTree b] -> TreeCursor a b -> [CTree b] -> Widget n) ->
+  ForestCursor a b ->
+  Widget n
 forestCursorWidget = foldForestCursor
--- cursor-brick/src/Cursor/Brick/Tree.hs
+++ cursor-brick/src/Cursor/Brick/Tree.hs
@@ -1,24 +1,97 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Cursor.Brick.Tree where
 
-import Cursor.Tree
-
 import Brick.Types
+import Brick.Widgets.Core
+import Cursor.Tree
+import qualified Data.List.NonEmpty as NE
+
+verticalPaddedTreeCursorWidgetM ::
+  forall a b n m.
+  Monad m =>
+  (a -> m (Widget n)) ->
+  (b -> m (Widget n)) ->
+  Int ->
+  TreeCursor a b ->
+  m (Widget n)
+verticalPaddedTreeCursorWidgetM goA goB padding = treeCursorWidgetM wrap cur
+  where
+    goCTree :: CTree b -> m (Widget n)
+    goCTree (CNode b cf) = do
+      top <- goB b
+      bot <- goCForest cf
+      pure $ top <=> padLeft (Pad padding) bot
+    goCForest :: CForest b -> m (Widget n)
+    goCForest = \case
+      EmptyCForest -> pure emptyWidget
+      ClosedForest _ -> pure emptyWidget
+      OpenForest nect -> fmap vBox $ traverse goCTree $ NE.toList nect
+    wrap :: [CTree b] -> b -> [CTree b] -> Widget n -> m (Widget n)
+    wrap lefts above rights curW = do
+      top <- goB above
+      bot <-
+        vBox
+          <$> sequenceA
+            ( concat
+                [ map goCTree lefts,
+                  [pure curW],
+                  map goCTree rights
+                ]
+            )
+      pure $ top <=> padLeft (Pad padding) bot
+    cur :: a -> CForest b -> m (Widget n)
+    cur a cf = do
+      top <- goA a
+      bot <- goCForest cf
+      pure $ top <=> padLeft (Pad padding) bot
+
+verticalPaddedTreeCursorWidget ::
+  forall a b n.
+  (a -> Widget n) ->
+  (b -> Widget n) ->
+  Int ->
+  TreeCursor a b ->
+  Widget n
+verticalPaddedTreeCursorWidget goA goB padding = treeCursorWidget wrap cur
+  where
+    goCTree :: CTree b -> Widget n
+    goCTree (CNode b cf) = goB b <=> padLeft (Pad padding) (goCForest cf)
+    goCForest :: CForest b -> Widget n
+    goCForest = \case
+      EmptyCForest -> emptyWidget
+      ClosedForest _ -> emptyWidget
+      OpenForest nect -> vBox $ map goCTree $ NE.toList nect
+    wrap :: [CTree b] -> b -> [CTree b] -> Widget n -> Widget n
+    wrap lefts above rights curW =
+      goB above
+        <=> padLeft
+          (Pad 2)
+          ( vBox $
+              concat
+                [ map goCTree lefts,
+                  [curW],
+                  map goCTree rights
+                ]
+          )
+    cur :: a -> CForest b -> Widget n
+    cur a cf = goA a <=> padLeft (Pad padding) (goCForest cf)
 
 treeCursorWidgetM ::
-     forall a b n m. Monad m
-  => ([CTree b] -> b -> [CTree b] -> Widget n -> m (Widget n))
-  -> (a -> CForest b -> m (Widget n))
-  -> TreeCursor a b
-  -> m (Widget n)
+  forall a b n m.
+  Monad m =>
+  ([CTree b] -> b -> [CTree b] -> Widget n -> m (Widget n)) ->
+  (a -> CForest b -> m (Widget n)) ->
+  TreeCursor a b ->
+  m (Widget n)
 treeCursorWidgetM = traverseTreeCursor
 
 treeCursorWidget ::
-     forall a b n.
-     ([CTree b] -> b -> [CTree b] -> Widget n -> Widget n)
-  -> (a -> CForest b -> Widget n)
-  -> TreeCursor a b
-  -> Widget n
+  forall a b n.
+  ([CTree b] -> b -> [CTree b] -> Widget n -> Widget n) ->
+  (a -> CForest b -> Widget n) ->
+  TreeCursor a b ->
+  Widget n
 treeCursorWidget = foldTreeCursor
