3a819b944ea0c4d18baf792f15790662470459df
Tom Sydney Kerckhove <syd@cs-syd.eu>
2023-08-21 14:46:21 +0200

    Upgrade to nixos-23.05 and LTS-20.25

--- flake.nix
+++ flake.nix
@@ -1,31 +1,29 @@
 {
   description = "cursor-brick";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-22.05";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-23.05";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
-    validity.url = "github:NorfairKing/validity?ref=flake";
+    validity.url = "github:NorfairKing/validity";
     validity.flake = false;
-    cursor.url = "github:NorfairKing/cursor?ref=flake";
+    cursor.url = "github:NorfairKing/cursor";
     cursor.flake = false;
+    nixpkgs-22_11.url = "github:NixOS/nixpkgs?ref=nixos-22.11";
     nixpkgs-22_05.url = "github:NixOS/nixpkgs?ref=nixos-22.05";
     nixpkgs-21_11.url = "github:NixOS/nixpkgs?ref=nixos-21.11";
-    nixpkgs-21_05.url = "github:NixOS/nixpkgs?ref=nixos-21.05";
   };
 
   outputs =
     { self
     , nixpkgs
+    , nixpkgs-22_11
     , nixpkgs-22_05
     , nixpkgs-21_11
-    , nixpkgs-21_05
-    , flake-utils
     , pre-commit-hooks
     , validity
     , cursor
     }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
     let
+      system = "x86_64-linux";
       pkgsFor = nixpkgs: import nixpkgs {
         inherit system;
         overlays = [
@@ -38,18 +36,18 @@
       pkgs = pkgsFor nixpkgs;
     in
     {
-      overlays = import ./nix/overlay.nix;
-      packages.default = pkgs.haskellPackages.cursor-brick;
-      checks =
+      overlays.${system} = import ./nix/overlay.nix;
+      packages.${system}.default = pkgs.haskellPackages.cursor-brick;
+      checks.${system} =
         let
           backwardCompatibilityCheckFor = nixpkgs:
             let pkgs' = pkgsFor nixpkgs;
             in pkgs'.haskellPackages.cursor-brick;
           allNixpkgs = {
             inherit
+              nixpkgs-22_11
               nixpkgs-22_05
-              nixpkgs-21_11
-              nixpkgs-21_05;
+              nixpkgs-21_11;
           };
           backwardCompatibilityChecks = pkgs.lib.mapAttrs (_: nixpkgs: backwardCompatibilityCheckFor nixpkgs) allNixpkgs;
         in
@@ -66,7 +64,7 @@
             };
           };
         };
-      devShells.default = pkgs.haskellPackages.shellFor {
+      devShells.${system}.default = pkgs.haskellPackages.shellFor {
         name = "cursor-brick-shell";
         packages = (p: with p; [
           cursor-brick
@@ -77,7 +75,7 @@
           niv
           zlib
           cabal-install
-        ] ++ (with pre-commit-hooks;
+        ] ++ (with pre-commit-hooks.packages.${system};
           [
             hlint
             hpack
@@ -87,5 +85,5 @@
           ]);
         shellHook = self.checks.${system}.pre-commit.shellHook;
       };
-    });
+    };
 }
